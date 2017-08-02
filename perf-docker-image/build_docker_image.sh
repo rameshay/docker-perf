ac23dcad908a9b8fe8871b2ac4f5b00c0b30bd92
Ramesh Ayyagari (CS) <ramesh_ayyagari@symantec.com>
2017-08-02 11:43:58 -0700

    Modified to build docker image based on jenkins variables

--- perf-docker-image/build_docker_image.sh
+++ perf-docker-image/build_docker_image.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 set -e
 IMAGE_NAME=`basename $PWD`
-if [ -z $1 ] ; then
+if [ -z ${PERF_DOCKER_IMAGE_TAG} ]; then 
+    echo "Please provide a proper tag value to the image that is being generated."
+    echo "Exiting ...."
+    exit 1
+fi
+
+if [ -z ${PERF_POLYGRAPH_VERSION} ] ; then
     ELASTICA_VERSION="latest"
 else
-    ELASTICA_VERSION="$1"
+    ELASTICA_VERSION=${PERF_POLYGRAPH_VERSION}
 fi
-sudo docker build --rm . -t "${IMAGE_NAME}":"${ELASTICA_VERSION}"
+sudo docker build --rm . -t "${PERF_DOCKER_IMAGE_TAG}/${IMAGE_NAME}":"${ELASTICA_VERSION}"
+exit $?
