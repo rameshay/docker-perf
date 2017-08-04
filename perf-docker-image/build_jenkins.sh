34cb7c08a6baff276bfc17cade15039fdddd951f
Ramesh Ayyagari (CS) <ramesh_ayyagari@symantec.com>
2017-08-03 17:50:53 -0700

    Darn these spaces

--- perf-docker-image/build_jenkins.sh
+++ perf-docker-image/build_jenkins.sh
@@ -19,8 +19,8 @@ function push_image() {
 
 function main() {
     IMAGE_NAME=`basename $PWD`
-    PERF_DOCKER_IMAGE_TAG = $1
-    PERF_POLYGRAPH_VERSION = $2
+    PERF_DOCKER_IMAGE_TAG=$1
+    PERF_POLYGRAPH_VERSION=$2
     if [ -z $PERF_DOCKER_IMAGE_TAG ]; then
         echo "Please provide a proper tag value to the image that is being generated."
         echo "Exiting ...."
