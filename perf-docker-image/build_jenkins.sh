8c5ca2d6943e2fd2b78893bb57b1dfd40bdcf150
Ramesh Ayyagari (CS) <ramesh_ayyagari@symantec.com>
2017-08-03 17:46:21 -0700

    Fixing run time error

--- perf-docker-image/build_jenkins.sh
+++ perf-docker-image/build_jenkins.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
 function build_image() {
+    echo "Building docker image with tag $1"
     sudo docker build --rm . -t $1
     exit $?
 }
 
 function push_image() {
+    echo "Pushing docker image with tag $1 to registry"
     # Commenting out this until the images are build clean
     #sudo docker push "${PERF_DOCKER_IMAGE_TAG}/${IMAGE_NAME}":"${ELASTICA_VERSION}"
     #if [ $? -eq 0 ];
@@ -29,7 +31,7 @@ function main() {
         ELASTICA_VERSION=${PERF_POLYGRAPH_VERSION}
     fi
     DOCKER_IMAGE_TAG="${PERF_DOCKER_IMAGE_TAG}/${IMAGE_NAME}":"${ELASTICA_VERSION}"
-    build_image($DOCKER_IMAGE_TAG)
-    push_image($DOCKER_IMAGE_TAG)
+    build_image $DOCKER_IMAGE_TAG
+    push_image $DOCKER_IMAGE_TAG
 }
 main $@
